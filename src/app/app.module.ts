80c04e1722d28eb6e15c4f1dd29b7f9fdecaed9b
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-02-07 08:10:45 +0300

    addition of profilr component

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ProfileService } from './services/profile.service';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileComponent,
+
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+   HttpClientModule
+
+  ],
+
+  providers: [ProfileService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/services/profile.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+  private username:string;
+  private clientid ='99332';
+  private clientsecret ='Iv1.3066706d2225c3c2';
+
+
+  constructor(private httpClient:HttpClient) {
+console.log('service is now ready!');
+this.username ='Jackson-coder-arch';
+   }
+   getProfileInfo(){
+     return this.httpClient.get("https://api.github.com/users/"+this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+     .pipe(map((response: any) => response.json()));
+      }
+}
