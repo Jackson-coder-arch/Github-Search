47e85de72e7a561cafbbff1b553498e7b9456c4e
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-02-08 19:42:56 +0300

    profile css

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../../services/profile.service';
+import {from} from 'rxjs';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
+
 export class ProfileComponent implements OnInit {
+  
   profile: any ;
   repos: any;
   username!:string;
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  clientsecret:'Iv1.3066706d2225c3c2'
+
 };
 
 /*
